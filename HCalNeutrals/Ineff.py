a37d42bab296874ed4e23e2e92761949ce89b79a
Matthew Solt <mrsolt@alumni.stanford.edu>
2021-01-06 12:58:23 -0500

    update ineff script

--- HCalNeutrals/Ineff.py
+++ HCalNeutrals/Ineff.py
@@ -47,10 +47,10 @@ infile3 = TFile(remainder[3])
 histo = infile.Get("myana/myana_lambda_max")
 nevents = histo.GetEntries()
 
-histo2 = infile2.Get("myana/myana_lambda_max")
+histo2 = infile2.Get("myana/myana_lambda_min")
 nevents2 = histo2.GetEntries()
 
-histo3 = infile3.Get("myana/myana_lambda_max")
+histo3 = infile3.Get("myana/myana_lambda_min")
 nevents3 = histo3.GetEntries()
 
 h = TH1F("h", "h", histo.GetNbinsX(), histo.GetXaxis().GetBinLowEdge(1), histo.GetXaxis().GetBinUpEdge(histo.GetNbinsX()))
@@ -72,21 +72,40 @@ for i in range(h3.GetNbinsX()):
     ineff = 1 - n / float(nevents3)
     h3.SetBinContent(i, ineff)
     
-#histo2 = infile.Get("myana/myana_lambda_max")
-#nevents2 = histo2.GetEntries()
+histo4 = infile.Get("myana/myana_lambda_max")
+nevents4 = histo4.GetEntries()
 
-#h2 = TH1F("h2", "h2", histo2.GetNbinsX(), histo2.GetXaxis().GetBinLowEdge(1), histo2.GetXaxis().GetBinUpEdge(histo2.GetNbinsX()))
-#for i in range(h2.GetNbinsX()):
-#    n = histo2.Integral(1,i)
-#    ineff = 1 - n / float(nevents2)
-#    h2.SetBinContent(i, ineff)
+histo5 = infile2.Get("myana/myana_lambda_max")
+nevents5 = histo5.GetEntries()
+
+histo6 = infile3.Get("myana/myana_lambda_max")
+nevents6 = histo6.GetEntries()
+
+h4 = TH1F("h4", "h4", histo4.GetNbinsX(), histo4.GetXaxis().GetBinLowEdge(1), histo4.GetXaxis().GetBinUpEdge(histo4.GetNbinsX()))
+h5 = TH1F("h5", "h5", histo5.GetNbinsX(), histo5.GetXaxis().GetBinLowEdge(1), histo5.GetXaxis().GetBinUpEdge(histo5.GetNbinsX()))
+h6 = TH1F("h6", "h6", histo6.GetNbinsX(), histo6.GetXaxis().GetBinLowEdge(1), histo6.GetXaxis().GetBinUpEdge(histo6.GetNbinsX()))
+
+for i in range(h4.GetNbinsX()):
+    n = histo4.Integral(1,i)
+    ineff = 1 - n / float(nevents4)
+    h4.SetBinContent(i, ineff)
+    
+for i in range(h5.GetNbinsX()):
+    n = histo5.Integral(1,i)
+    ineff = 1 - n / float(nevents5)
+    h5.SetBinContent(i, ineff)
+    
+for i in range(h6.GetNbinsX()):
+    n = histo6.Integral(1,i)
+    ineff = 1 - n / float(nevents6)
+    h6.SetBinContent(i, ineff)
     
 openPDF(outfile,c)
 h.SetLineColor(1)
 h2.SetLineColor(2)
 h3.SetLineColor(4)
 h.Draw()
-h.SetTitle("Inefficiency")
+h.SetTitle("Inefficiency at Min Z")
 h.GetXaxis().SetTitle("#lambda")
 h.GetYaxis().SetTitle("Inefficiency")
 h2.Draw("same")
@@ -104,9 +123,16 @@ legend.Draw()
 c.SetLogy(1)
 c.Print(outfile+".pdf")
 
-#h2.Draw()
-#h2.SetTitle("Inefficiency")
-#h2.GetXaxis().SetTitle("#lambda")
-#h2.GetYaxis().SetTitle("Inefficiency")
-#c.Print(outfile+".pdf")
+h4.SetLineColor(1)
+h5.SetLineColor(2)
+h6.SetLineColor(4)
+h4.Draw()
+h4.SetTitle("Inefficiency at Max Z")
+h4.GetXaxis().SetTitle("#lambda")
+h4.GetYaxis().SetTitle("Inefficiency")
+h5.Draw("same")
+h6.Draw("same")
+legend.Draw()
+c.Print(outfile+".pdf")
+
 closePDF(outfile,c)
